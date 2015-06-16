b2f52692b04b7a3f8468a20ddae0661cbd25edac
grahnz <git@johangrahn.se>
2015-06-16 04:02:21 +0200

    Added files

---
+++ TwitchChatExample.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(TwitchIRC))]
+public class TwitchChatExample : MonoBehaviour
+{
+    public int maxMessages = 100; //we start deleting UI elements when the count is larger than this var.
+    private LinkedList<GameObject> messages =
+        new LinkedList<GameObject>();
+    public UnityEngine.UI.InputField inputField;
+    public UnityEngine.UI.Button submitButton;
+    public UnityEngine.RectTransform chatBox;
+    public UnityEngine.UI.ScrollRect scrollRect;
+    private TwitchIRC IRC;
+    //when message is recieved from IRC-server or our own message.
+    void OnChatMsgRecieved(string user, string msg)
+    {
+        if (messages.Count > maxMessages)
+        {
+            Destroy(messages.First.Value);
+            messages.RemoveFirst();
+        }
+
+        GameObject go = new GameObject("twitchMsg");
+        var text = go.AddComponent<UnityEngine.UI.Text>();
+        var layout = go.AddComponent<UnityEngine.UI.LayoutElement>();
+        go.transform.parent = chatBox;
+        messages.AddLast(go);
+
+        layout.minHeight = 20f;
+        text.text = "<b>" + user + "</b>" + ": " + "<color=#32323E>" + msg + "</color>";
+        text.color = ColorFromUsername(user);
+        text.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+        scrollRect.velocity = new Vector2(0, 1000f);
+    }
+    //when Submit button is clicked or ENTER is pressed.
+    public void OnSubmit()
+    {
+        if (inputField.text.Length > 0)
+        {
+            IRC.SendMessage(inputField.text);
+            //invoke onchatmsgrecieved because the IRC server wont send the message to the sending client.
+            OnChatMsgRecieved(IRC.nickName, inputField.text); //for local GUI only.
+            inputField.text = "";
+        }
+    }
+    Color ColorFromUsername(string username)
+    {
+        Random.seed = username.Length + (int)username[0] + (int)username[username.Length - 1];
+        return new Color(Random.Range(0.25f, 0.55f), Random.Range(0.20f, 0.55f), Random.Range(0.25f, 0.55f));
+    }
+    // Use this for initialization
+    void Start()
+    {
+        IRC = this.GetComponent<TwitchIRC>();
+        IRC.messageRecievedEvent.AddListener(OnChatMsgRecieved);
+    }
+}
---
+++ TwitchIRC.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TwitchIRC : MonoBehaviour
+{
+    public string oath;
+    public string nickName;
+    public string channelName;
+    private string server = "irc.twitch.tv";
+    private int port = 6667;
+
+    //event(nick,msg).
+    public class MsgEvent : UnityEngine.Events.UnityEvent<string, string> { }
+    public MsgEvent messageRecievedEvent = new MsgEvent();
+
+    [HideInInspector()]
+    public string buffer;
+    private bool stopThread = false;
+    private Queue<string> commandQueue = new Queue<string>();
+    private List<KeyValuePair<string, string>> chatMsgs = new List<KeyValuePair<string, string>>();
+    private void IRCProcedure()
+    {
+        System.Net.Sockets.TcpClient sock = new System.Net.Sockets.TcpClient();
+        System.IO.TextReader input;
+        System.IO.TextWriter output;
+        //Connect to irc server and get input and output text streams from TcpClient.
+        sock.Connect(server, port);
+        if (!sock.Connected)
+        {
+            Debug.Log("Failed to connect!");
+            return;
+        }
+        input = new System.IO.StreamReader(sock.GetStream());
+        output = new System.IO.StreamWriter(sock.GetStream());
+
+        //Send PASS & NICK.
+        output.WriteLine("PASS " + oath);
+        output.WriteLine("NICK " + nickName.ToLower());
+        output.Flush();
+
+        //Process each line received from irc server
+        while (!stopThread)
+        {
+            buffer = input.ReadLine();
+            if (buffer == null)
+                continue;
+            //Debug.Log(buffer);
+            //Send pong reply to any ping messages
+            if (buffer.StartsWith("PING "))
+            {
+                output.Write(buffer.Replace("PING", "PONG") + "\r\n");
+                output.Flush();
+            }
+            if (buffer[0] != ':')
+                continue;
+
+            //was user message?
+            if (buffer.Contains("PRIVMSG #"))
+            {
+                int msgIndex = buffer.IndexOf("PRIVMSG #");
+                string msg = buffer.Substring(msgIndex + channelName.Length + 11);
+                string user = buffer.Substring(1, buffer.IndexOf('!') - 1);
+
+                OnMessageRecieved(user, msg);
+                chatMsgs.Add(new KeyValuePair<string, string>(user, msg));
+            }
+            //do we have any commands to send?
+            lock (commandQueue)
+            {
+                if (commandQueue.Count > 0)
+                {
+                    output.WriteLine(commandQueue.Peek());
+                    output.Flush();
+                    commandQueue.Dequeue();
+                }
+            }
+            //After server sends 001 command, we can join a channel
+            if (buffer.Split(' ')[1] == "001")
+            {
+                output.WriteLine("JOIN #" + channelName);
+                output.Flush();
+            }
+        }
+    }
+    public void SendCommand(string cmd)
+    {
+        lock (commandQueue)
+        {
+            commandQueue.Enqueue(cmd);
+        }
+    }
+    public void SendMessage(string msg)
+    {
+        lock (commandQueue)
+        {
+            commandQueue.Enqueue("PRIVMSG #" + channelName + " :" + msg);
+        }
+    }
+    void OnMessageRecieved(string nick, string msg)
+    {
+        Debug.Log(nick + ": " + msg);
+    }
+
+    //MonoBehaviour Events.
+    void Start()
+    {
+        System.Threading.Thread T = new System.Threading.Thread(IRCProcedure);
+        stopThread = false;
+        T.Start();
+    }
+    void OnDestroy()
+    {
+        stopThread = true;
+    }
+    void OnDisable()
+    {
+        stopThread = true;
+    }
+    void Update()
+    {
+        lock (chatMsgs)
+        {
+            if (chatMsgs.Count > 0)
+            {
+                for (int i = 0; i < chatMsgs.Count; i++)
+                {
+                    messageRecievedEvent.Invoke(chatMsgs[i].Key, chatMsgs[i].Value);
+                }
+                chatMsgs.Clear();
+            }
+        }
+    }
+}
